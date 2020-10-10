ccfda69021e881a72946421cb08e6da5ad295d38
mrcobias <marcelocobias18@gmail.com>
2020-10-10 02:19:16 +0000

    adcição de classes no projeto

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 require_once(__DIR__ . '/../../config.php');
 
+global $DB, $PAGE, $OUTPUT, $USER;
+
 require_login();
 
 $context = context_system::instance();
@@ -13,12 +15,30 @@ $html = '';
 
 if (is_siteadmin($USER->id)) {
 
+    // Load libraries.
+    require_once('xerata_form.php');
 
+    // Load calculate params from form, request or set default values.
+    $mform = new xereta_local_selection_form($url, null, 'get');
 
+    if ($mform->is_submitted()) {
+        // Params from form post.
+        $formdata = $mform->get_data();
+        $mintime = $formdata->mintime;
+        $maxtime = $formdata->maxtime;
+    } else {
+        // Params from request or default values.
+        $mintime = optional_param('mintime', time(), PARAM_INT);
+        $maxtime = optional_param('maxtime', time(), PARAM_INT);
+        $mform->set_data(array('mintime' => $mintime, 'maxtime' => $maxtime));
+    }
+    echo $OUTPUT->header();
 
+    // Form.
+    $mform->display();
+
+    echo $OUTPUT->box_start();
 
 }
 
-echo $OUTPUT->header();
-echo $html;
 echo $OUTPUT->footer();
--- lang/en/local_xereta.php
+++ lang/en/local_xereta.php
@@ -1,2 +1,6 @@
 <?php
 $string['pluginname'] = 'Xereta';
+$string['form_text'] = 'Selecione o usuario e o periodo que deseja xeretar';
+$string['mintime'] = 'Inicio';
+$string['maxtime'] = 'Fim';
+$string['submit'] = 'Xeretar';
